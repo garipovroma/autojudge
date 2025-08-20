ce02a0e1d8ca3f826a6e3fce7bda673182067a0d
garipovroma <devilgar@gmail.com>
2025-08-20 18:12:54 +0300

    cleanup src/find_important_tokens_lcb.py

--- src/find_important_tokens_lcb.py
+++ src/find_important_tokens_lcb.py
@@ -30,10 +30,6 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-prompt_with_8_shots = "Given the following problem, reason and give a final answer to the problem.\nProblem: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n There are 15 trees originally. Then there were 21 trees after some more were planted. So there must have been 21 - 15 = 6. The final answer is 6\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5. The final answer is 5\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n Originally, Leah had 32 chocolates. Her sister had 42. So in total they had 32 + 42 = 74. After eating 35, they had 74 - 35 = 39. The final answer is 39\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n Jason started with 20 lollipops. Then he had 12 after giving some to Denny. So he gave Denny 20 - 12 = 8. The final answer is 8\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n Shawn started with 5 toys. If he got 2 toys each from his mom and dad, then that is 4 more toys. 5 + 4 = 9. The final answer is 9\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n There were originally 9 computers. For each of 4 days, 5 more computers were added. So 5 * 4 = 20 computers were added. 9 + 20 is 29. The final answer is 29\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n Michael started with 58 golf balls. After losing 23 on tuesday, he had 58 - 23 = 35. After losing 2 more, he had 35 - 2 = 33 golf balls. The final answer is 33\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?\nYour response should end with \"The final answer is [answer]\" where [answer] is the response to the problem.\n Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3 = 15 dollars. So she has 23 - 15 dollars left. 23 - 15 is 8. The final answer is 8\n\nGiven the following problem, reason and give a final answer to the problem.\nProblem: "
-prompt_with_0_shots = "Given the following problem, reason and give a final answer to the problem.\n"
-formatting_prompt = "Your response should end with \"The final answer is [answer]\" where [answer] is the response to the problem."
-
 
 def verify_args(args):
     assert args.process_id < args.world_size, "--process_id must be < --world_size"
@@ -71,20 +67,6 @@ def get_args():
     parser.add_argument('--process_saver_id', type=int, default=0)
     parser.add_argument('--total_gpus', type=int, default=8)
     parser.add_argument('--lcb_path', type=str, default='none')
-
-    # class args:
-    #     draft_model = 'meta-llama/Llama-3.2-1B-Instruct'
-    #     target_model = 'meta-llama/Llama-3.1-8B-Instruct'
-    #     # target_model = 'meta-llama/Llama-3.2-1B-Instruct'
-    #     torch_dtype = 'auto'
-    #     gsm8k_train_path = '../data/gsm8k_train.json'
-    #     random_seed = 42
-    #     max_new_tokens = 2048
-    #     output_folder = 'lcb_test_data'
-    #     output_file = 'important_tokens'
-    #     num_shots = 0
-    #     n_tasks = 50
-    #     live_code_bench_data_dir = 'live_code_bench_data'
 
     args = parser.parse_args()
     verify_args(args)
@@ -136,10 +118,6 @@ def find_important_tokens(
     draft_score = test_program(draft_response, eval_samples, tokenizer)['score']
 
     logger.info(f'{target_score=}, {draft_score=}, difficulty={sample.difficulty}')
-
-    # if draft_score == 1 or target_score == 0:
-    #     logger.info('Skipped')
-    #     return dict(draft_score=draft_score, target_score=target_score)
 
     current_response = target_response  # to be updated by algorithm
 
